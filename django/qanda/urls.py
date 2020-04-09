7bab036cf3f5847dc1dba8356cdd3ed2e467c522
mason <masonkim32@gmail.com>
2020-04-09 21:26:28 +0200

    feat: add QuestionDetailView to the URLConf

--- django/qanda/urls.py
+++ django/qanda/urls.py
@@ -6,5 +6,14 @@ from qanda import views
 app_name = 'qanda'
 
 urlpatterns = [
-    path('ask', views.AskQuestionView.as_view(), name='ask'),
+    path(
+        'ask',
+        views.AskQuestionView.as_view(),
+        name='ask'
+    ),
+    path(
+        'q/<int:pk>',
+        views.QuestionDetailView.as_view(),
+        name='question_detail'
+    ),
 ]
